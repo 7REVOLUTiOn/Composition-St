fbe2044d7f4745197f27cf2a81c615ab32ce601a
Egor Solodnikov <dogdoor66@gmail.com>
2022-11-18 03:07:02 +0300

    Initial ViewBinding in fragments

--- app/src/main/java/com/example/composition_st/presentation/GameFragment.kt
+++ app/src/main/java/com/example/composition_st/presentation/GameFragment.kt
@@ -6,15 +6,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.composition_st.R
+import com.example.composition_st.databinding.FragmentGameBinding
+import java.lang.RuntimeException
 
 class GameFragment : Fragment() {
+
+    private var _binding: FragmentGameBinding? = null
+    //Чтобы избежать крашей, мы до создания view - обьявили ее до создания view и после view
+    // Таким образом к binding можно образаться только когда она существует
+    private val binding: FragmentGameBinding
+    //Чтобы не делать постоянные проверки на null, мы создаем не null переменную binding,
+    //у которой переопредили геттер.
+    //Этот гетер возращает значение переменной binding с нижним подчеркиванием, если оно не равно null,
+    //а если ровно то у нас бросается исключение
+        get() =_binding ?: throw RuntimeException("FragmentGameBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        _binding = FragmentGameBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/com/example/composition_st/presentation/WelcomeFragment.kt
+++ app/src/main/java/com/example/composition_st/presentation/WelcomeFragment.kt
@@ -6,14 +6,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.composition_st.R
+import com.example.composition_st.databinding.FragmentWelcomeBinding
+import java.lang.RuntimeException
 
 class WelcomeFragment : Fragment() {
 
+    private var _binding:FragmentWelcomeBinding? = null//Создаем обьект binding, который хранит ссылки на view элементы
+    private val binding:FragmentWelcomeBinding //Это сгенерируемый класс из кокоторых достаются view элементы
+        get()= _binding?: throw RuntimeException("FragmentWelcomeBinding == null")
+    //В случае если мы обратись к этой переменной когда она равна null
+    //мы бросаем исключение, что переменная binding равна null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+                              savedInstanceState: Bundle?): View {
+        _binding = FragmentWelcomeBinding.inflate(inflater,container,false) //После чего у сгенериуемого класса передаем метод inflate
+        // Сверху получаем экземпляр этого обьекта
+        // По итогу создаем обекта типа view, который сам под копотом найдет все обьекты по их id,
+        // создав для них ссылки
+        return binding.root //Получаем и возращаем view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonUnderstand.setOnClickListener{
 
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
