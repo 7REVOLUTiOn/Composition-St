cc42203e2bd61acb62866aa94adab0de562abd1f
Egor Solodnikov <dogdoor66@gmail.com>
2022-11-20 19:47:45 +0300

    Initial GameFinishedFragment.The main development is finished. Further improvements are only possible.

--- app/src/main/java/com/example/composition_st/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/example/composition_st/presentation/GameFinishedFragment.kt
@@ -36,12 +36,8 @@ class GameFinishedFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true){
-            override fun handleOnBackPressed() {
-                retryGame()
-            }
-        }) //Мы получаем ссылку на activity, у этой активити получим onBackPressedDispather
-            // И добоавим слушателель на клик кнопки назад, и при нажатии вызывается метод retry game
+        setupClickListener()
+        bindViews()
     }
 
     override fun onDestroyView() {
@@ -55,6 +51,57 @@ class GameFinishedFragment : Fragment() {
 
     private fun retryGame(){
          requireActivity().supportFragmentManager.popBackStack(GameFragment.NAME,FragmentManager.POP_BACK_STACK_INCLUSIVE)
+    }
+
+    private fun setupClickListener(){
+        val callback = object : OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+                retryGame()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner,callback)
+        binding.buttonRetry.setOnClickListener{
+            retryGame()
+        }//Мы получаем ссылку на activity, у этой активити получим onBackPressedDispather
+        // И добоавим слушателель на клик кнопки назад, и при нажатии вызывается метод retry game
+    }
+
+    private fun bindViews(){
+        with(binding){
+            emojiResult.setImageResource(getSmileResId())
+            tvRequiredAnswers.text = String.format(
+                getString(R.string.required_score),
+                gameResult.gameSettings.minCountOfRightAnswers
+            )
+            tvScoreAnswers.text = String.format(
+                getString(R.string.score_answers),
+                gameResult.countOfRightAnswers
+            )
+            tvRequiredPercentage.text = String.format(
+                getString(R.string.required_percentage),
+                gameResult.gameSettings.minPercentOfRightAnswers
+            )
+            tvScorePercentage.text = String.format(
+                getString(R.string.score_percentage),
+                getPercentOfRightAnswers()
+            )
+        }
+    }
+
+    private fun getSmileResId():Int{
+        return if (gameResult.winner){
+            R.drawable.ic_smile
+        } else {
+            R.drawable.ic_sad
+        }
+    }
+
+    private fun getPercentOfRightAnswers() = with(gameResult){
+        if (countOfQuestion == 0){
+            0
+        } else{
+            ((countOfRightAnswers / countOfQuestion.toDouble()) * 100).toInt()
+        }
     }
 
 
